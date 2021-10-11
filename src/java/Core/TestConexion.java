6d4ba32cf6060c9f42767802ac154c8bcdb6d73f
Miguel Villalba <mike.mavc27@gmai.com>
2021-10-10 22:21:18 -0300

    Init Commit

---
+++ src/java/Core/TestConexion.java
@@ -0,0 +1,21 @@
+package Core;
+
+//import Modelo.Dto.MenuSistemaDTO;
+
+
+public class TestConexion {
+    private Conexion conexion;
+ //   private MenuSistemaDTO dto;
+    
+    
+    public TestConexion() {
+        conexion = new Conexion();
+      
+    }
+
+
+    public static void main(String[] args) {
+        new TestConexion();
+    }
+    
+}
